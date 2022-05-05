8990fd65a216fd627a7d3646ca8f301bad686be9
Gabriel Espinoza <31670639+gspinoza@users.noreply.github.com>
2022-05-05 17:00:13 -0400

    added api get request to get place info

--- server/routes/handler.js
+++ server/routes/handler.js
@@ -33,6 +33,24 @@ function cleanData (data) {
     return JSON.stringify(placesArray)
 }
 
+function cleanPlaceDetails(data) {
+  var opentripmapdata =  JSON.parse(JSON.stringify(data))
+
+  // data to return
+  let newPlaceObject = {};
+  house_number = opentripmapdata['address']['house_number'];
+  road = opentripmapdata['address']['road'];
+  suburb = opentripmapdata['address']['suburb'];
+  city = opentripmapdata['address']['city'];
+  postcode = opentripmapdata['address']['postcode'];
+  newPlaceObject.address = `${house_number} ${road}, ${suburb}, ${city} ${postcode}`;
+  newPlaceObject.url = opentripmapdata['url'];
+  newPlaceObject.wikipedia_url = opentripmapdata['wikipedia'];
+  newPlaceObject.image = opentripmapdata['image'];
+  newPlaceObject.wiki_info = opentripmapdata['wikipedia_extracts']['text'];
+  // console.log(newPlaceObject);
+  return JSON.stringify(newPlaceObject)
+}
 
 const cors = require('cors')
 router.use(cors())
@@ -47,12 +65,23 @@ router.get('/geo/:place', (req, res) => {
         .catch(error => console.log('error', error))
 })
 
+// get nyc place
 router.get('/nyc/places/:radius/:limit', (req, res) => {
   // make external request
   var requestOptions = {method: 'GET',redirect: 'follow'};
   fetch(`http://api.opentripmap.com/0.1/en/places/radius?apikey=${api_key}&radius=${req.params.radius}&limit=${req.params.limit}&kinds=natural,historic,cultural&rate=3&offset=10&lon=-74.0059&lat=40.71427`, requestOptions)
     .then(response => response.json())
     .then(data => res.end(cleanData(data))) // return
+    .catch(error => console.log('error', error));
+});
+
+// get place info
+router.get('/nyc/place/details/:placeid', (req, res) => {
+  // make external request
+  var requestOptions = {method: 'GET',redirect: 'follow'};
+  fetch(`http://api.opentripmap.com/0.1/en/places/xid/${req.params.placeid}?apikey=${api_key}`, requestOptions)
+    .then(response => response.json())
+    .then(data => res.end(cleanPlaceDetails(data))) // return
     .catch(error => console.log('error', error));
 });
 
